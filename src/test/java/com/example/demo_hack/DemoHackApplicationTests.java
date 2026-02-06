628516d951caa532c1b4fe9fe1774d7ca5c5106d
Meganathan-18 <rmeganathann@gmail.com>
2026-02-06 05:39:00 +0000

    Initial Spring Boot setup

---
+++ src/test/java/com/example/demo_hack/DemoHackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_hack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
